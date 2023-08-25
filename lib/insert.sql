447adc2f37b67f1fd97a101ec65ca0c48bbd2019
user <ephrahim.lenaiayasa@student.moringaschool.com>
2023-08-25 04:11:15 +0300

    Completed assighnment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Mr Chocolate", 30, "M", "Black", "aggressive", "True");
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Rowdy", 10, "M", "Brown", "gentle", "True");
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Tabitha", 6, "F", "Brown", "curious", "True");
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Sergeant Brown", 67, "F", "White", "shy", "True");
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Melissa", 13, "M", "Black", "gentle", "True");
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Grinch", 2, "F", "White", "Playful", "True");
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Wendy", 6, "M", "Black", "Mature", "True");
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(NULL, 39, "F", "White", "aggressive", "True");
+
